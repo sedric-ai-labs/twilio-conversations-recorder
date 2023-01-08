b4629a8c2f03272ec908e64c12fa248560788145
omerderazon92 <omerderazon92@gmail.com>
2023-01-08 10:33:54 +0200

    Initial commit

---
+++ src/ConversationsRecorderPlugin.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FlexPlugin } from '@twilio/flex-plugin';
+
+const PLUGIN_NAME = 'ConversationsRecorderPlugin';
+
+export default class ConversationsRecorderPlugin extends FlexPlugin {
+  constructor() {
+    super(PLUGIN_NAME);
+  }
+
+  /**
+   * This code is run when your plugin is being started
+   * Use this to modify any UI components or attach to the actions framework
+   *
+   * @param flex { typeof import('@twilio/flex1-ui') }
+   */
+  async init(flex, manager) {
+    flex.Actions.addListener("beforeAcceptTask", (payload) => {
+      payload.conferenceOptions.record = 'true';
+      
+      const task = payload.task._task
+      const workerSID = task._worker.sid;
+      const direction = task.attributes.direction;
+      const caller = task.attributes.caller.replace("+", "@");
+      const called = task.attributes.caller.replace("+", "@");
+      
+      const callbackURL = `https://us-central1-sedric-dev.cloudfunctions.net/flex_api?workerSID=${workerSID}&direction=${direction}&called=${called}&caller=${caller}`;
+      payload.conferenceOptions.recordingStatusCallback = callbackURL;
+    });
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import * as FlexPlugin from '@twilio/flex-plugin';
+
+import ConversationsRecorderPlugin from './ConversationsRecorderPlugin';
+
+FlexPlugin.loadPlugin(ConversationsRecorderPlugin);
